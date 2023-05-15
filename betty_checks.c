c983a7966b94180d21463014e935739ebad7e380
plopapy1 <plopapy@gmail.com>
2023-05-15 10:07:31 -0700

    betty checker

---
+++ betty_checks.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - simple additcion
+  *
+  * Return: reture zero (0)
+  *
+  */
+
+int main(void)
+{
+	int a = 3;
+	int b = 4;
+	int c;
+
+	if (a != 0)
+	{
+		c = b + a
+		printf("c equal %d\n" c);
+	}
+
+	return(0)
+}
