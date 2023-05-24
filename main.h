d4c61f687b7ee16b21c3a69d8cc3490e85f5dc1f
Doctorstrange <doctorstrange247@gmail.com>
2023-05-24 17:49:44 -0500

    first main push with 4 function files

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int count_spaces(const char *input);
+
+int file_exist(const char *filename);
+
+int file_in_path_exist(const char *filename);
+
+char *pre_fix(char *str);
+
+void ter_minate(int sig);
+
+char **av_buffer(int ac, char *str);
+
+char *b_pre_buffer(char *tok);
+
+int what_if(char *av[]);
+
+#endif
---
+++ s_2shell.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <signal.h>
+#include "main.h"
+/**
+ *free_end - function to free malloc space
+ *@buf: pointer one
+ *@buff: pointer two
+ *@bufff: pointer three
+ *Return: Nothing
+ */
+void free_end(char *buf, char *buff, char *bufff)
+{
+	free(buf);
+	free(buff);
+	free(bufff);
+}
+
+/**
+ *main - the main function
+ *@argc: number of arguments
+ *@argv: pointer to array of arguments
+ *Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	size_t n = 0;
+	ssize_t getstat;
+	char *chk_file, *o_cmd;
+char *str = NULL, *delim = " \n", *token, *pre_pre, *b_pre, **av = NULL;
+	int ac, count, chk_int, echo_chk;
+
+	signal(SIGINT, ter_minate);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+	getstat = getline(&str, &n, stdin);
+	if (getstat == -1)
+		break;
+	if (str[0] == '\n')
+		continue;
+	ac = count_spaces(str);
+	av = av_buffer(ac, str);
+	token = strtok(str, delim);
+		if (strcmp("exit", token) == 0)
+		break;
+	b_pre = b_pre_buffer(token);
+	strcpy(b_pre, token);
+	o_cmd = b_pre_buffer(b_pre);
+	strcpy(o_cmd, b_pre);
+	chk_file = b_pre;
+	chk_int = file_exist(chk_file);
+	echo_chk = file_in_path_exist(chk_file);
+	pre_pre = pre_fix(b_pre);
+	count = 0;
+	av[0] = pre_pre;
+	while (count < (ac - 1) && argc <= ac)
+	{
+		count++;
+		token = strtok(NULL, delim);
+		av[count] = token;
+	}
+	count++;
+	av[count] = NULL;
+	if (chk_int == 0 && isatty(STDIN_FILENO))
+		what_if(av);
+	else if (echo_chk == 0 && isatty(!STDIN_FILENO))
+		what_if(av);
+	else
+		printf("%s: 1: %s: not found\n", argv[0], o_cmd);
+	wait(NULL);
+	free(av);
+}
+	free_end(o_cmd, b_pre, str);
+	return (0);
+}
